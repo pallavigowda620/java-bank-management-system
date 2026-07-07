a49ac3870a23e8141177427f17281de7cee896cc
Pallavi Gowda <pallavigouda1@gmail.com>
2026-07-07 23:22:07 +0530

    Implement BankAccount class and account creation feature

---
+++ src/BankAccount.java
@@ -0,0 +1,50 @@
+public class BankAccount {
+    private int accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(int accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+
+    }
+    public int getAccountNumber(){
+        return accountNumber;
+    }
+
+    public String getAccountHolderName(){
+        return accountHolderName;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public void deposit(double amount){
+        if(amount > 0 ){
+            balance += amount;
+            System.out.println("Deposited ₹" + amount + " Sucessfully!");
+        } else {
+            System.out.println("Deposit amount must  be greater than zero.");
+        
+        }
+
+    }
+
+    public void withdraw(double amount){
+        if(amount <= 0){
+           System.out.println("Withdrawl amount should be greater than zero");
+        }else if(amount > balance){
+            System.out.println("Insufficient balance!");
+        
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawn ₹" + amount + " Successfully!");
+        }
+        
+    }
+
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static ArrayList<BankAccount> accounts = new ArrayList<>();
+
+    public static void main(String[] args) {
+        boolean running = true;
+        
+        while (running) {
+            System.out.println("Welcome to the Bank Account Management System");
+            
+            System.out.println("1. Create a new account");
+            System.out.println("2. View all acounts");
+            System.out.println("3. Search account by account number");
+            System.out.println("4. Deposit money");
+            System.out.println("5. Withdraw money");
+            System.out.println("6. Check balance");
+            System.out.println("7. Delete account");
+            System.out.println("8. Exit");
+
+            System.out.println("Please select an option: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    //createAccount();
+                    break;
+
+                case 2:
+                    //viewAccounts();
+                    break;
+
+                case 3:
+                    //searchAccount();
+                    break;
+
+                case 4:
+                    //depositMoney();
+                    break;
+
+                case 5:
+                    //withdrawMoney();
+                    break;
+
+                case 6:
+                    //checkBalance();
+                    break;
+
+                case 7:
+                    //deleteAccount();
+                    break;
+
+                case 8:
+                    running = false;
+                    System.out.println("Exiting the system. Goodbye!");
+                    break;
+
+                default:
+                    System.out.println("Invalid option. Please try again.");
+
+
+            }
+
+            public static void createAccount() {
+                System.out.print("Enter account number: ");
+                int accountNumber = sc.nextInt();
+                sc.nextLine();
+
+                System.out.print("Enter account holder name: ");
+                String accountHolderName = sc.nextLine();
+
+                System.out.print("Enter initial balance: ");
+                double balance = sc.nextDouble();
+
+                BankAccount newAccount = new BankAccount(accountNumber, accountHolderName, balance);
+                accounts.add(newAccount);
+                System.out.println("Account created sucessfully!");
+                
+            }
+
+            
+
+
+
+        }
+    }
+}
